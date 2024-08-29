817ea949853b42e7758ffe682373864f92057693
TheRealDigão <71913765+TheRealDigao@users.noreply.github.com>
2024-08-28 21:32:53 -0300

    Create exec-8.py

---
+++ exec-8.py
@@ -0,0 +1,5 @@
+#7) Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
+ano = int(input("Sua idade em anos: "))
+mês = int(input("Sua idade em meses: "))
+dia = int(input("Sua idade em dias: "))
+print("Essa é a sua idade em dias: ", dia)
