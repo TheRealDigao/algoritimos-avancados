94e1f5689f3080b0626a868c25fbd19064391f16
TheRealDigão <71913765+TheRealDigao@users.noreply.github.com>
2024-08-30 02:51:32 -0300

    Update exec-8.py

--- exec-8.py
+++ exec-8.py
@@ -5,11 +5,11 @@ brancos = int(input("Qual a quantidade de votos em branco? "))
 nulos = int(input("Nulos? "))
 validos = int(input("Válidos? "))
 
-percentual_brancos = (eleitores/brancos)*100
-percentual_nulos = (eleitores/nulos)*100
-percentual_validos = (eleitores/validos)*100
+percentual_brancos = (brancos/eleitores)*100
+percentual_nulos = (nulos/eleitores)*100
+percentual_validos = (validos/eleitores)*100
 
 print("- Total de porcentagens -")
-print("- Total de votos válidos: ", percentual_brancos, "% -")
-print("- Total de votos em branco: ", percentual_nulos, "% -")
-print("- Total de votos nulos: ", percentual_validos, "% -")
+print("- Total de votos em branco: ", percentual_brancos, "% -")
+print("- Total de votos nulos: ", percentual_nulos, "% -")
+print("- Total de votos válidos: ", percentual_validos, "% -")
